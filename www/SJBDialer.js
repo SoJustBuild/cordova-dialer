a8c27c93fc4583d6fb09c4f46fc77e83e7f59e30
Ozgur Susoy <ozgur@sojustbuild.com>
2014-03-02 01:28:43 +1100

    grr!

--- www/SJBDialer.js
+++ www/SJBDialer.js
@@ -3,7 +3,7 @@ var SJBDialer = function() {}
 SJBDialer.prototype.dial = function(phnum) {
 	alert("dial "+phnum);
 	try{
-    cordova.exec("PhoneDialer.dialPhone", {"number" : phnum });
+    cordova.exec("SJBDialer.dialPhone", {"number" : phnum });
 	}catch(e){alert("dial failed"+e);}
 };
 
